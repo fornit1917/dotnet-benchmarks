cbcf33b4b57d9d491f38f612c2f7ed852749cd02
Vitaliy Sushkov <fornit1917@gmail.com>
2025-06-15 18:51:34 +0300

    Update NpgsqlParamsBenchmark

--- MyBenchmarks/MyBenchmarks/NpgsqlParamsBenchmark.cs
+++ MyBenchmarks/MyBenchmarks/NpgsqlParamsBenchmark.cs
@@ -9,11 +9,13 @@ public class NpgsqlParamsBenchmark
     private readonly NpgsqlDataSource _dataSource = NpgsqlDataSource.Create("Host=localhost;Username=test_user;Password=12345;Database=test_db");
 
     private const string PositionalCommandText = @"
-        INSERT INTO test_table (data_1, data_2) VALUES ($1, $2)
+        INSERT INTO test_table_2 (data_1, data_2, data_3, data_4, data_5) 
+        VALUES ($1, $2, $3, $4, $5)
     ";
 
     private const string NamedParamsCommandText = @"
-        INSERT INTO test_table(data_1, data_2) VALUES (@first_value, @second_value)
+        INSERT INTO test_table_2 (data_1, data_2, data_3, data_4, data_5) 
+        VALUES (@value_1, @value_2, @value_3, @value_4, @value_5)
     ";
 
     [Benchmark(Baseline = true)]
@@ -25,8 +27,11 @@ public class NpgsqlParamsBenchmark
         {
             Parameters =
             {
-                new("first_value", Guid.NewGuid().ToString()),
-                new("second_value", Guid.NewGuid().ToString()),
+                new("value_1", Guid.NewGuid().ToString()),
+                new("value_2", Guid.NewGuid().ToString()),
+                new("value_3", Guid.NewGuid().ToString()),
+                new("value_4", Guid.NewGuid().ToString()),
+                new("value_5", Guid.NewGuid().ToString()),
             }
         };
         cmd.ExecuteNonQuery();
@@ -43,6 +48,9 @@ public class NpgsqlParamsBenchmark
             {
                 new() { Value = Guid.NewGuid().ToString() },  // $1
                 new() { Value = Guid.NewGuid().ToString() },  // $2
+                new() { Value = Guid.NewGuid().ToString() },  // $3
+                new() { Value = Guid.NewGuid().ToString() },  // $4
+                new() { Value = Guid.NewGuid().ToString() },  // $5
             }
         };
         cmd.ExecuteNonQuery();
